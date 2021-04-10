1ba7cfd2912604a967803b8278f09b6160ed45ad
0xFeelix <mahammalaxov@gmail.com>
2021-04-10 15:35:33 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <Navbar />
-  <css-doodle click-to-update class="lol">
-    :doodle{ @grid: 40x1 / 12em; grid-gap: 5px; } @keyframes slide-in { 0% {
-    transform: translateY(-100%); } 100% { transform: traslateY(0); } }
-    background: @pick(#EFBB35, #dfa612, #FAE042, #EFBB35, #E84279, #dfa612);
-    width:100%; height: @rand(10%, 100%); transition: opacity .2s ease-out;
-    animation: slide-in .25s ease-out; opacity: @pick(0.05, 0.1, 0.2, 0.3, 0.4);
-    :hover{opacity: 1}
-  </css-doodle>
+  <Doodle />
 
-  <div class="container text-light main-content">
+
+  <div class="container text-light cat-head">
     <router-view />
   </div>
 </template>
@@ -19,9 +13,9 @@
 
 <script>
 import Navbar from "@/components/Navbar";
-
+import Doodle from "@/components/Doodle";
 export default {
-  components: { Navbar },
+  components: { Navbar, Doodle },
 };
 </script>
 
@@ -39,12 +33,12 @@ body {
   font-family: "New Tegomin" !important;
   background: #21272f !important;
 }
-.lol {
-  position: absolute;
-  z-index: 1;
-  width: 100%;
+
+.container-fluid{
+  padding: 0 !important;
 }
-.main-content {
-  margin-top: 25vh;
+
+.cat-head {
+  margin-top: 25vh !important;
 }
 </style>
---
+++ src/components/Doodle.vue
@@ -0,0 +1,18 @@
+<template>
+    <css-doodle click-to-update class="doodle">
+        :doodle{ @grid: 40x1 / 12em; grid-gap: 5px; } @keyframes slide-in { 0% {
+        transform: translateY(-100%); } 100% { transform: traslateY(0); } }
+        background: @pick(#EFBB35, #dfa612, #FAE042, #EFBB35, #E84279, #dfa612);
+        width:100%; height: @rand(10%, 100%); transition: opacity .2s ease-out;
+        animation: slide-in .25s ease-out; opacity: @pick(0.05, 0.1, 0.2, 0.3, 0.4);
+        :hover{opacity: 1}
+    </css-doodle>
+</template>
+
+<style>
+.doodle {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+}
+</style>
