d348e7fbef0f911cb325a6ccc0791400481fc815
0xFeelix <mahammalaxov@gmail.com>
2021-04-18 15:33:27 +0300

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
-    
+    <button @click="handleClick()" class="btn btn-warning">Click</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  
-}
+  name: "Home",
+
+  setup() {
+    
+    const handleClick = () => {
+      console.log("true");
+    };
+
+    return { handleClick };
+  },
+};
 </script>
 
 
 <style>
-
 </style>
